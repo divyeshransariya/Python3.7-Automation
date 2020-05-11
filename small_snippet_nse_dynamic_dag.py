8e0f6879d15e0148f52300527af26754f582dff9
divyeshransariya <59407037+divyeshransariya@users.noreply.github.com>
2020-05-12 02:01:08 +0530

    Update small_snippet_nse_dynamic_dag.py

--- small_snippet_nse_dynamic_dag.py
+++ small_snippet_nse_dynamic_dag.py
@@ -1,35 +1,37 @@
-for bt in server_tasks[srv].strat_tasks:
-for dependent_task in task_dict[bt].depends:
-    if dependent_task.startswith('__dag'):
-        if not re.match("^__dag:.*;__task:.+",dependent_task):
-            raise ValueError('{} does not follow __dag:.*(can be empty);__task:.+(can not be empty) like mapping, so change it in nse_task_config'.format(dependent_task))
-        dag_info,task_info = dependent_task.split(';')
-        task_id = task_info.split(':')[1]
-        dag_id = dag_info.split(':')[1]
-        if dag_id == "":
-            if task_id in (cm_info[0] + transfer_task_suffix, fo_info[0] + transfer_task_suffix):
-                has_cmfo_use = True
-                dag.set_dependency(tune_server.task_id, bt)
-            if task_id == cd_info[0] + transfer_task_suffix:
-                has_cd_use = True
-                dag.set_dependency(tune_server.task_id, bt)
-            dag.set_dependency(task_id,bt)
-        else:
-            if dag.has_task(task_id + '_downstream'):
-                dag.set_dependency(task_id + '_downstream', bt)
+for strategy in server_tasks[srv].strat_tasks:
+    # checking Dependency for each strat 
+    for dependent_task in task_dict[strategy].depends:
+        if dependent_task.startswith('__dag'):
+            if not re.match("^__dag:.*;__task:.+", dependent_task):
+                raise ValueError('{} does not follow __dag:.*(can be empty);__task:.+(can not be empty) like mapping, so change it in nse_task_config'.format(dependent_task))
+            dag_obj, task_obj = dependent_task.split by ";"
+            task_id = task id form task_obj
+            dag_id = dag id form dag_obj
+            if dag_id is empty :
+                if task_id present in cm/fo instgen task :
+                    has_cmfo_use = True
+                    dag.set_dependency(tune_server.task_id, strategy) # First tune server after then startegy run
+                if task_id is part of CD instgen task:
+                    has_cd_use = True
+                    dag.set_dependency(tune_server.task_id, strategy)
+                dag.set_dependency(task_id, strategy) #First that task run from current dag then strat will run
             else:
-                instance = infiles_path >> ExternalTaskSensor(
-                    task_id = task_id + '_downstream',
+                instance = Task_T >> ExternalTaskSensor(
+                    task_id = <Task name>,
                     external_dag_id = dag_id,
                     external_task_id = task_id,
                     mode = 'reschedule')
-                dag.set_dependency(task_id + '_downstream', bt)
-    else :
-        if dependent_task == '':
-            raise ValueError('dependent_task is an empty string,please give some task name')
-        if dependent_task in server_tasks[srv].strat_tasks:
-            dag.set_dependency(dependent_task, bt)
-        elif dependent_task in alias_dict :
-            dag.set_dependency(alias_dict[dependent_task], bt)
-        else:
-            raise ValueError('{} is not listed in strat_tasks or depedent_alias of strat_tasks of server {} please fix it'.format(dependent_task, srv))
+                dag.set_dependency(task_id + '_downstream', strategy)
+        else :
+            # If no dependent task present then throw an error
+            if dependent_task == '':
+                raise ValueError('dependent_task is an empty string, please give some task name')
+            # In case Start X depend on Start Y then
+            if dependent_task in server_tasks[srv].strat_tasks:
+                dag.set_dependency(dependent_task, strategy)
+            # Here alias_dict represent alise for specific strategy, from alias task list no
+            # two strategy depend on same alias list
+            elif dependent_task in alias_dict :
+                dag.set_dependency(alias_dict[dependent_task], strategy)
+            else:
+                raise ValueError('{} is not listed in strat_tasks or depedent_alias of strat_tasks of server {} please fix it'.format(dependent_task, srv))
