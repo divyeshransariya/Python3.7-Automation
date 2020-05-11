a41ad76293f764504c175c89f2f66e932191d774
divyeshransariya <59407037+divyeshransariya@users.noreply.github.com>
2020-05-12 01:24:19 +0530

    Create nse_task_config.py

---
+++ nse_task_config.py
@@ -0,0 +1,16 @@
+from datetime import time
+
+from common.server_task_info import TaskInfo
+
+# For export environmental variables...
+exports_map={ }
+
+nse_tasks_dict={
+    'Strategy_X': TaskInfo(exec_path='$base_code_path/< path of strat object file >/$build_mode/....',
+        build_mode='release',
+        depends=['__dag:;__task:task_T', '__dag:;__task:<Depends of instrument tranfer task>'],
+        depend_alias=['Start Y'],
+        maintainer_slack=[],
+        telemetry_check_expr='<Some telemetry Expression to check strat X running correctly>',
+        onload=True, start_time=<Time for when strat X will run on server>, end_time=<After this time strat X should be stop>),
+}
