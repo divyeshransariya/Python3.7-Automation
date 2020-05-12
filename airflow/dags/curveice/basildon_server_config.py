27d0f35ace4c769add013b17a36911153c34b996
divyeshransariya <divyesh.ransariya@silverleafcaps.com>
2020-05-12 16:25:23 +0530

    Removed actual code from This repo

--- airflow/dags/curveice/basildon_server_config.py
+++
@@ -1,7 +0,0 @@
-from common.server_task_info import ServerInfo
-
-server_tasks = {
-    'basildon': ServerInfo(
-        ['ice_fo_feed', 'curve_fo_feed','ice_adapt', 'pbStrat', 'sonia_strat', 'sonia_one', 
-            'transporter', 'sonia_transporter','ice_instgen','curve_instgen'],False,30,True,True),
-}
